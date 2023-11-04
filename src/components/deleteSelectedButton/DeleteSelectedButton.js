8c2e29c623442ae6a2e5c2ffe014f1b9a6f4c442
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-11-04 07:46:29 +0530

    fix : delete selected button role warning

--- src/components/deleteSelectedButton/DeleteSelectedButton.js
+++ src/components/deleteSelectedButton/DeleteSelectedButton.js
@@ -3,7 +3,6 @@ import React from "react";
 const DeleteSelectedButton = ({ handleDeleteSelected, selectedRows }) => {
   return (
     <button
-      role="button"
       className="btn-delete-selected"
       onClick={() => handleDeleteSelected()}
       disabled={selectedRows.length > 0 ? false : true}
